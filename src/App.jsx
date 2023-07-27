9c91e6ac41fdcbfd388acc8839bb8a097d16f7f1
AmairT <amair0829@gmail.com>
2023-07-27 09:46:27 -0700

    code for my react form

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
+import SignUpForm from "./components/SignUpForm"
+import Authenticate from "./components/Authenticate"
+import { useState } from "react";
 import './App.css'
 
-function App() {
+export default function App() {
+  const [token, setToken] = useState(null);
 
   return (
     <>
-      <div>
-      
-      </div>
-    
+      <SignUpForm token = {token} setToken = {setToken}/>
+      <Authenticate token = {token} setToken = {setToken} />
     </>
-  )
+  );
 }
-
-export default App
